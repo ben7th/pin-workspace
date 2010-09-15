e54df63b4ff39ee5e3bfd4c9b142ebb12828a813
fushang318 <fushang318@gmail.com>
2010-09-15 13:56:35 +0800

    忽略列表好用么

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,15 +5,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-case RAILS_ENV
-when 'production'
   ActionController::Base.session = {
-    :key=>'_mindpin_ei_session',
+    :key=>'_mindpin_2010_session_dev',
     :secret=>'883abe7844502ee307e376fa4d0509253d7f9e55fc8be69a934735cd470cc8671af39e27482885960f3364fa8af420b5519571193e22987c3e9e4f9da29f15fb'
   }
-else
-  ActionController::Base.session = {
-    :key=>'_mindpin_ei_session_dev',
-    :secret=>'883abe7844502ee307e376fa4d0509253d7f9e55fc8be69a934735cd470cc8671af39e27482885960f3364fa8af420b5519571193e22987c3e9e4f9da29f15fb'
-  }
-end
