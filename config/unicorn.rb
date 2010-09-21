fbe839157ad521597f72f884893e1d97a9050594
fushang318 <fushang318@gmail.com>
2010-09-21 13:37:54 +0800

    增加 unicorn 日志

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,9 @@ worker_processes 3
 listen '/tmp/nginx/sockets/workspace_unicorn.sock', :backlog => 2048
 timeout 30
 
+stderr_path("#{File.dirname(__FILE__)}/../log/unicorn_error.log")
+stdout_path("#{File.dirname(__FILE__)}/../log/unicorn.log")
+
 pid_file = "/web/2010/pids/unicorn_pin_workspace.pid"
 pid pid_file
 preload_app true
