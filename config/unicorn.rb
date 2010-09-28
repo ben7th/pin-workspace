e7695b0268d2ae5e7945333fa94bf3b93df42324
ben7th <ben7th@sina.com>
2010-09-28 17:29:09 +0800

    修改配置文件、忽略列表以及配置文件

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,34 @@
+# worker 数量
 worker_processes 3
 
-listen '/web/2010/sockets/workspace_unicorn.sock', :backlog => 2048
-timeout 30
+# 日志位置
+stderr_path("/web/2010/logs/unicorn-workspace-error.log")
+stdout_path("/web/2010/logs/unicorn-workspace.log")
 
-stderr_path("#{File.dirname(__FILE__)}/../log/unicorn_error.log")
-stdout_path("#{File.dirname(__FILE__)}/../log/unicorn.log")
-
-pid_file = "/web/2010/pids/unicorn_pin_workspace.pid"
-pid pid_file
+# 加载 超时设置 监听
 preload_app true
+timeout 30
+listen '/web/2010/sockets/unicorn-workspace.sock', :backlog => 2048
+
+pid_file_name = "/web/2010/pids/unicorn-workspace.pid"
+pid pid_file_name
+
+# REE GC
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
 
 before_fork do |server, worker|
-  old_pid = pid_file + '.oldbin'
+  old_pid = pid_file_name + '.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
+      # ...
     end
   end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
 end
